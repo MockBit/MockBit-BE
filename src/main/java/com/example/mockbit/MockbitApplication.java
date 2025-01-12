15e04b3e037d5fb9aeade95d10e1af3d302378d0
justgotothedesk <wlscjf3877@naver.com>
2025-01-12 16:52:28 +0900

    feat: Spring 설정 세팅

---
+++ src/main/java/com/example/mockbit/MockbitApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mockbit/MockbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
