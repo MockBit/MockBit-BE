68cbd89de8420b0fc6e6cd5b3f45dfb6012af121
justgotothedesk <wlscjf3877@naver.com>
2025-01-26 17:41:33 +0900

    feat: 사용자 ID 유효성 검증을 위한 Userid 클래스 추가
    - 사용자 ID 길이 제한 검증 (8자 이상, 14자 이하).
    - ID 값을 캡슐화하여 Value Object로 설계.
    - 중복 ID 검증을 위한 구조 준비.

---
+++ src/main/java/com/example/mockbit/user/domain/Userid.java
@@ -0,0 +1,51 @@
+package com.example.mockbit.user.domain;
+
+import com.example.mockbit.common.exception.MockBitException;
+import com.example.mockbit.common.exception.MockbitErrorCode;
+import com.example.mockbit.user.domain.repository.UserRepository;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+@Embeddable
+public class Userid {
+
+    public static final int ID_MAX_NAME_LENGTH = 14;
+    private static final int ID_MIN_NAME_LENGTH = 8;
+
+    @Column(nullable = false, length = ID_MAX_NAME_LENGTH)
+    private String value;
+
+    public Userid(String value) {
+        validateUserid(value);
+        this.value = value;
+    }
+
+//    public Userid(String value, UserRepository userRepository) {
+//        validateUserid(value);
+//        checkDuplicateUserid(value, userRepository);
+//        this.value = value;
+//    }
+
+    private void validateUserid(String userid) {
+        int useridLength = userid.length();
+        if (useridLength < ID_MIN_NAME_LENGTH || useridLength > ID_MAX_NAME_LENGTH) {
+            throw new MockBitException(
+                    MockbitErrorCode.USER_ID_LENGTH_INVALID,
+                    ID_MIN_NAME_LENGTH, ID_MAX_NAME_LENGTH
+            );
+        }
+    }
+
+//    private void checkDuplicateUserid(String userid, UserRepository userRepository) {
+//        if (userRepository.findByUserid(userid).isPresent()) {
+//            throw new MockBitException(MockbitErrorCode.USER_ID_ALREADY_EXIST);
+//        }
+//    }
+}
