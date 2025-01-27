094bdd19f0d789793516ed31ba1631de28c87219
justgotothedesk <wlscjf3877@naver.com>
2025-01-27 17:40:24 +0900

    feat: 사용자 컨트롤러에 CRUD 기능 추가
    - 회원 생성 기능 추가.
    - 회원 조회 기능 추가.
    - 회원 정보 업데이트 기능 추가.
    - 회원 삭제 기능 추가.

---
+++ src/main/java/com/example/mockbit/user/presentation/UserController.java
@@ -0,0 +1,51 @@
+package com.example.mockbit.user.presentation;
+
+import com.example.mockbit.common.auth.Login;
+import com.example.mockbit.event.application.response.UserAppResponse;
+import com.example.mockbit.user.application.UserService;
+import com.example.mockbit.user.application.request.UserJoinAppRequest;
+import com.example.mockbit.user.application.request.UserUpdateAppRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Long> createUser(
+            @Valid @RequestBody UserJoinAppRequest request
+    ) {
+        Long id = userService.join(request);
+        return ResponseEntity.ok(id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserAppResponse> getUser(@PathVariable Long id) {
+        UserAppResponse user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> updateUser(
+            @Login Long id,
+            @Valid @RequestBody UserUpdateAppRequest request
+    ) {
+        userService.updateUser(request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
