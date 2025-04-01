b5d6f8f82164756d885d0fb85463136d405a767d
justgotothedesk <wlscjf3877@naver.com>
2025-04-01 21:52:29 +0900

    delete: 지정가 주문 로직 변경으로 인해 사용하지 않는 `toOrder` 메서드 제거 (#17)

--- src/main/java/com/example/mockbit/order/application/request/OrderAppRequest.java
+++ src/main/java/com/example/mockbit/order/application/request/OrderAppRequest.java
@@ -1,9 +1,6 @@
 package com.example.mockbit.order.application.request;
 
-import com.example.mockbit.order.domain.Order;
 import lombok.Builder;
-
-import java.time.Instant;
 
 @Builder
 public record OrderAppRequest(
@@ -14,18 +11,4 @@ public record OrderAppRequest(
         String position,
         String sellOrBuy
 ) {
-
-    public Order toOrder(Long userId) {
-        return Order.builder()
-                .id(String.format("Orders:%s:%d", price, userId))
-                .price(price)
-                .userId(userId)
-                .orderedAt(String.valueOf(Instant.now()))
-                .btcPrice(btcPrice)
-                .orderPrice(orderPrice)
-                .leverage(leverage)
-                .position(position)
-                .sellOrBuy(sellOrBuy)
-                .build();
-    }
 }
